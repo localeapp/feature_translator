b2f44d06bc4a6f5d2e69785feed769f4329f391d
Christopher Dell <chris@tigrish.com>
2011-05-05 20:51:22 +0200

    Added some basic validations to Category and Feature

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :features
+  validates_uniqueness_of :key
+  validates_format_of :key, :with => /^\w+$/
 
   def name(target_locale=nil)
     I18n.t "models.category.data.key.#{key}", :locale => target_locale
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,7 @@
 class Feature < ActiveRecord::Base
   belongs_to :category
+  validates_uniqueness_of :key
+  validates_format_of :key, :with => /^\w+$/
 
   def name(target_locale=nil)
     I18n.t "models.feature.data.key.#{key}", :locale => target_locale
