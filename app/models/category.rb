ad1f4b504bea491bf2b5e940f13db5bea7e63466
Christopher Dell <chris@tigrish.com>
2011-05-05 19:22:37 +0200

    Move Category and Feature names into #name methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :features
+
+  def name(target_locale=nil)
+    I18n.t "models.category.data.key.#{key}", :locale => target_locale
+  end
 end
--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,3 +1,7 @@
 class Feature < ActiveRecord::Base
   belongs_to :category
+
+  def name(target_locale=nil)
+    I18n.t "models.feature.data.key.#{key}", :locale => target_locale
+  end
 end
